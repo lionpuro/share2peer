a63103ba73da90674fa10e011cc75613c84eb67b
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2026-01-06 11:03:17 +0200

    fix incorrectly typed field in session schema
    Include null in the possible types of clients since it can be null
    in certain situations.

--- frontend/src/lib/session.ts
+++ frontend/src/lib/session.ts
@@ -6,7 +6,7 @@ import { ClientSchema } from "./client";
 export const SessionSchema = z.object({
 	id: z.string(),
 	host: z.optional(z.string()),
-	clients: z.optional(z.array(ClientSchema)),
+	clients: z.optional(z.union([z.array(ClientSchema), z.null()])),
 });
 
 export type Session = z.infer<typeof SessionSchema>;

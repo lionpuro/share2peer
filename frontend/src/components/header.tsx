64064e8f1cf4b5e1a03c24b2062a37f8b9ea97b7
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2026-01-12 14:10:07 +0200

    adjust logo size

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,8 +4,8 @@ import { IconAccessPoint } from "#/components/icons";
 export function Header() {
 	return (
 		<header className="mx-auto flex w-full max-w-screen-lg items-center px-6 py-4 sm:px-8">
-			<Link to="/" className="flex items-center gap-1 text-xl font-bold">
-				<IconAccessPoint className="size-8 text-primary" />
+			<Link to="/" className="flex h-8 items-center gap-1 text-xl font-bold">
+				<IconAccessPoint className="size-7 text-primary" />
 				share2peer
 			</Link>
 		</header>

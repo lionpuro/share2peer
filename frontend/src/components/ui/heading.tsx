8ab804604ad6134c2ed4b79958e1fd449226b28a
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2026-03-04 11:29:02 +0200

    adjust heading component base styles

--- frontend/src/components/ui/heading.tsx
+++ frontend/src/components/ui/heading.tsx
@@ -13,22 +13,32 @@ type Props = HTMLAttributes<HTMLHeadingElement> & {
 };
 
 export function Heading({ order, size, className, children, ...props }: Props) {
+	const classes = "leading-none";
 	switch (order) {
 		case 1:
 			return (
-				<h1 className={cn(size ? sizes[size] : sizes.lg, className)} {...props}>
+				<h1
+					className={cn(size ? sizes[size] : sizes.lg, classes, className)}
+					{...props}
+				>
 					{children}
 				</h1>
 			);
 		case 2:
 			return (
-				<h2 className={cn(size ? sizes[size] : sizes.md, className)} {...props}>
+				<h2
+					className={cn(size ? sizes[size] : sizes.md, classes, className)}
+					{...props}
+				>
 					{children}
 				</h2>
 			);
 		case 3:
 			return (
-				<h3 className={cn(size ? sizes[size] : sizes.sm, className)} {...props}>
+				<h3
+					className={cn(size ? sizes[size] : sizes.sm, classes, className)}
+					{...props}
+				>
 					{children}
 				</h3>
 			);

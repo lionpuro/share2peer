a459822605769ab41a437df6011cc62a72c151a9
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2026-03-07 11:36:32 +0200

    reduce height of header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -3,7 +3,7 @@ import { IconAccessPoint } from "#/components/icons";
 
 export function Header() {
 	return (
-		<header className="mx-auto flex w-full max-w-screen-xl items-center px-6 py-4 sm:px-8">
+		<header className="mx-auto flex w-full max-w-screen-xl items-center px-6 py-3 sm:px-8">
 			<Link to="/" className="flex items-center gap-1 text-lg font-semibold">
 				<IconAccessPoint className="size-6.5 text-primary" />
 				WebSend

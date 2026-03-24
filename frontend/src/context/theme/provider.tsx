b2ddecbb32aa95c398181add9aeeb8e6c1a6c2f5
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2026-03-24 13:51:21 +0200

    fix preferred color scheme detection

--- frontend/src/context/theme/provider.tsx
+++ frontend/src/context/theme/provider.tsx
@@ -5,7 +5,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
 	const [theme, setTheme] = useState<"light" | "dark">(() => {
 		const value = localStorage.getItem("theme");
 		if (value !== "light" && value !== "dark") {
-			const dark = window.matchMedia("(prefers-color-scheme: dark)");
+			const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 			return dark ? "dark" : "light";
 		}
 		return value;
